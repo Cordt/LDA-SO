35f45e834e5938434e0d7b7c5377db02bfea9e8e
Cordt Voigt <cordt.voigt@tum.de>
2015-08-05 12:20:23 +0200

    Little changes to run on VM

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ setting = {
     # Filter all documents that appear in more than the given fraction of documents
     'filter_more_than_fraction_of_documents': 0.5,
 
-    'create_tag_cloud': True,
+    'create_tag_cloud': False,
     'clean_similarities_table': False,
 
     # Data used for topic model
@@ -48,7 +48,7 @@ setting = {
     # 1: Exact match distance
     # 2: Deviation distance
     # 3: Squared deviation distance
-    'distance_metric': 1}
+    'distance_metric': 2}
 
 if theme is 'reuters':
     # setting['folderprefix'] = '../../data/' + theme + '/'
@@ -57,36 +57,37 @@ else:
     # setting['folderprefix'] = '../../data/' + theme + '.stackexchange.com/'
     setting['folderprefix'] = '/srv/cordt-mt/data/' + theme + '.stackexchange.com/'
 
+for mIndex in range (2, 4, 1):
+    setting['distance_metric'] = mIndex
+    for index in range(1, 4, 1):
+        setting['data_for_model'] = index
 
-for index in range(1, 3, 1):
-    setting['data_for_model'] = index
+        # Topic model
+        tm = Topicmodel(setting)
+        tm.createmodel()
 
-    # Topic model
-    tm = Topicmodel(setting)
-    tm.createmodel()
+        # Create tag cloud
+        if setting['create_tag_cloud']:
+            Tagcloud.createtagcloud(tm, setting)
 
-    # Create tag cloud
-    if setting['create_tag_cloud']:
-        Tagcloud.createtagcloud(tm, setting)
+        # Create similarities tables
+        tm.determine_question_answer_distances()
 
-    # Create similarities tables
-    tm.determine_question_answer_distances()
+        # Create document lengths db if neccessary
+        directory = setting['resultfolder'] + setting['theme'] + "/model/"
+        filename = "lengths.db"
+        dbpath = ''.join([directory, filename])
 
-    # Create document lengths db if neccessary
-    directory = setting['resultfolder'] + setting['theme'] + "/model/"
-    filename = "lengths.db"
-    dbpath = ''.join([directory, filename])
+        if not os.path.isfile(dbpath):
+            logging.info("Document lengths database does not exist yet, determining lengths and writing to database...")
+            tm.determine_answer_lengths()
 
-    if not os.path.isfile(dbpath):
-        logging.info("Document lengths database does not exist yet, determining lengths and writing to database...")
-        tm.determine_answer_lengths()
+        # Determine avergae distances of answers, that are related to the question
+        tm.get_true_answers_distances(no_of_questions=-1)
 
-    # Determine avergae distances of answers, that are related to the question
-    tm.get_true_answers_distances(no_of_questions=-1)
+        # Determine the distance to the correct order of answers to a question, given by the upvote score,
+        # compared to the order
+        # given by the topic model
+        tm.compute_answer_order_metric(no_of_questions=-1)
 
-    # Determine the distance to the correct order of answers to a question, given by the upvote score,
-    # compared to the order
-    # given by the topic model
-    tm.compute_answer_order_metric(no_of_questions=-1)
-
-    tm.compute_answer_length_impact(no_of_questions=-1)
+        tm.compute_answer_length_impact(no_of_questions=-1)
